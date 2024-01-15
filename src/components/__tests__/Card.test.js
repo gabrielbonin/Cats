eda35e3de56f94fef9886c7f88fdb5121d23b140
Gabriel MBLABS <gabrielmblabs@MacBook-Pro-de-Gabriel.local>
2024-01-15 11:13:06 -0300

    improve card structure and css

--- src/components/__tests__/Card.test.js
+++ src/components/__tests__/Card.test.js
@@ -1,25 +1,43 @@
 import { render, screen } from '@testing-library/react';
 import Card from '../Card';
 
+const cardProps = {
+  name: 'Garfield',
+  phone: '111-111-11111',
+  email: 'bonin@gmail.com',
+  image: {
+    url: 'https://images.unsplash.com/photo-1533738363-b7f9aef128ce?q=80&w=2835&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    alt: 'cute cat',
+  },
+  favoured: false,
+};
+
 describe('Card', () => {
   test('should show name of cat', () => {
-    render(
-      <Card
-        name="Garfield"
-        phone="555-555"
-        email="bonin@gmail.com"
-        image={{
-          url: 'https://images.unsplash.com/photo-1533738363-b7f9aef128ce?q=80&w=2835&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-          alt: 'cute cat',
-        }}
-        favoured={false}
-      />,
-    );
+    render(<Card {...cardProps} />);
 
     expect(
       screen.getByRole('heading', {
         name: /garfield/i,
       }),
     ).toBeInTheDocument();
+  });
+
+  test('should show phone number', () => {
+    render(<Card {...cardProps} />);
+
+    expect(screen.getByText(/111-111-11111/i)).toBeInTheDocument();
+  });
+
+  test('should show email', () => {
+    render(<Card {...cardProps} />);
+
+    expect(screen.getByText(/bonin@gmail.com/i)).toBeInTheDocument();
+  });
+
+  test('should show image with correct src', () => {
+    render(<Card {...cardProps} />);
+
+    expect(screen.getByAltText(/cute cat/i).src).toBe(cardProps.image.url);
   });
 });
