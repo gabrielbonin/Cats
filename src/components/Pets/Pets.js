b43404a10fa7f9e30c951f11bfd9e9e55a50c725
Gabriel MBLABS <gabrielmblabs@MacBook-Pro-de-Gabriel.local>
2024-02-16 18:59:55 -0300

    improve more tests

--- src/components/Pets/Pets.js
+++ src/components/Pets/Pets.js
@@ -1,37 +1,31 @@
-import "./Pets.css"
-import Filter from "../Filter/Filter";
-import Cards from "../Cards/Cards"
+import axios from 'axios';
+import './Pets.css';
 
-import axios from "axios";
-import { useEffect, useState } from "react";
+import Cards from '../Cards/Cards';
 
-
+import { useEffect, useState } from 'react';
 
 const Pets = () => {
-  const [cats, setCats] = useState([])
+  const [cats, setCats] = useState([]);
 
   const fetchCats = async () => {
-    const response = axios.get("http://localhost:4000/cats");
-    setCats(response.data)
-  }
+    const response = axios.get('http://localhost:4000/cats');
+    setCats(response.data);
+  };
 
   useEffect(() => {
     fetchCats();
   }, []);
 
-  console.log('cats: ', cats)
+  console.log('cats: ', cats);
 
   return (
     <div className="container">
       <div className="app-container">
-
-
         <Cards cats={cats} />
-
       </div>
     </div>
   );
-
 };
 
 export default Pets;
